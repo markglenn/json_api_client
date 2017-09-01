890dfb93717451622374c3009569b7a7227be6aa
Julian Skinner <jskinner@decisiv.com>
2017-09-01 13:24:56 -0500

    Create a Note based on Elixir Map.

--- lib/decisiv/api_client/notes.ex
+++ lib/decisiv/api_client/notes.ex
@@ -1,10 +1,37 @@
 defmodule ApiClient.Notes do
+  @api_details %{scheme: "http", host: "localhost", port: 3112, version: "v1"}
+  @endpoint_name "notes"
 
   def all do
-    case HTTPoison.get("http://localhost:3116/v1/assets") do
+    case HTTPoison.get(url) do
      {:ok, res} -> {:ok, Poison.decode!(res.body)["data"]}
      {:error, err} -> {:error, :service_unavailable}
     end
   end
 
+  @doc """
+  Create a Note based on data map
+
+  Returns `%{:ok, response}`
+  """
+  def create(data) do
+    case HTTPoison.post(url, Poison.encode!(data), content_type) do
+      {:ok, res} -> {:ok, Poison.decode!(res.body)["data"]}
+      {:error, err} -> {:error, err}
+    end
+  end
+
+  defp url do
+    "#{scheme}://#{host}:#{port}/#{version}/#{endpoint}"
+  end
+
+  defp content_type do
+    %{"Content-type" => "application/json"}
+  end
+
+  defp scheme,   do: @api_details[:scheme]
+  defp host,     do: @api_details[:host]
+  defp port,     do: @api_details[:port]
+  defp version,  do: @api_details[:version]
+  defp endpoint, do: @endpoint_name
 end
