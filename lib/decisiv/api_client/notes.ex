29715f81491984dc4a050a2e1d6fa07ce124c935
Julian Skinner <jskinner@decisiv.com>
2017-09-01 23:12:27 -0500

    List all method accepts parameters for different filtering
    Can pass different parameters for the following:
    - page_size
    - page_num
    - sort
    - fields
    - filter

--- lib/decisiv/api_client/notes.ex
+++ lib/decisiv/api_client/notes.ex
@@ -1,8 +1,27 @@
 defmodule ApiClient.Notes do
   @api_details %{scheme: "http", host: "localhost", port: 3112, version: "v1"}
+  @defaults [page_size: nil, page_num: nil, filter: nil, sort: nil, fields: nil]
   @endpoint_name "notes"
 
-  def all do
+  @doc """
+  List all the NotesTest
+
+  params:
+    options: Set of options that are available, with no specific order
+      page_size: int
+      page_num: int
+      filter:
+      fields: %{}
+
+  ## Example
+    ApiClient.Notes.all()
+    ApiClient.Notes.all(page_size: 10)
+    ApiClient.Notes.all(page_size: 10, fields: %{notes: "id,topic,recipients"})
+  """
+  def all(options \\ []) do
+    options = generate_keyword_list(options)
+    query_params = build_query_params(options)
+
     case HTTPoison.get(url(), headers(), options()) do
      {:ok, res} -> {:ok, decoded_body_data(res)}
      {:error, _err} -> {:error, :service_unavailable}
@@ -42,6 +61,61 @@ defmodule ApiClient.Notes do
 
   defp options do
     [timeout: Decisiv.ApiClient.timeout(), recv_timeout: Decisiv.ApiClient.timeout()]
+  end
+
+  defp generate_keyword_list(options) do
+    Keyword.merge(@defaults, options)
+      |> Enum.into(%{})
+      |> Enum.reject(fn {_,v} -> is_nil(v) end) # remove all nil values
+  end
+
+  defp build_query_params(options) do
+    unless Enum.empty?(options) do
+      [head | tail] = options
+        |> List.keysort(0) # sort so that we get fields first. BRITTLE!!!
+
+      # do we have a fields tuple
+      output = if Enum.member?(Tuple.to_list(head), :fields) do
+          generate_fields_param(head)
+        else # head is just a tuple with key / value, convert to a list, and encode
+          encode_key_value(head)
+        end
+
+      q = unless Enum.empty?(tail) do
+        tail
+          |> URI.encode_query # encode them for query_string
+      end
+
+      [output, q]
+        |> Enum.filter(fn(v) -> v != nil end)
+        |> Enum.intersperse("&")
+        |> List.to_string
+    else
+      ""
+    end
+  end
+
+  @doc """
+    Generates an URI encoded string based on key/value pairs
+
+    Returns `key=value&key2=value2`
+  """
+  defp encode_key_value(tuple) do
+    tuple
+      |> Tuple.to_list
+      |> Enum.chunk(2)
+      |> Enum.reduce(%{}, fn([key, value], accumulator) -> Map.put(accumulator, key, value) end)
+      |> URI.encode_query # encode them for query_string
+  end
+  @doc """
+  Generates query string for fields based on map
+
+  Returns `fields[notes]=id,recipients`
+  """
+  defp generate_fields_param(fields_tuple) do
+    elem(fields_tuple, 1)
+    |> Enum.map(fn({key, value}) -> "fields[#{key}]=#{String.trim(value)}" end)
+    |> Enum.join("&") #
   end
 
   defp encode_data(data) do
