601c0efeb36726ade1cc9e98ecd31c2d7cf0e60d
Michael Lagutko <mlagutko@decisiv.net>
2017-10-10 11:05:14 +0300

    [AEG-336] Added tests for Relationships Object for index request

--- test/jsonapi_client/parser_index_test.exs
+++ test/jsonapi_client/parser_index_test.exs
@@ -2,7 +2,7 @@ defmodule ParserIndexTest do
   use ExUnit.Case
   doctest JsonApiClient.Parser, import: true
 
-  alias JsonApiClient.{Document, Resource, Parser, JsonApiProtocol, Links}
+  alias JsonApiClient.{Document, Resource, Parser, JsonApiProtocol, Links, Relationship, ResourceIdentifier}
 
   @protocol JsonApiProtocol.document_object()
 
@@ -99,6 +99,184 @@ defmodule ParserIndexTest do
           last: "http://example.com/articles?page[number]=13&page[size]=1"
         }
       }} = Parser.parse(document_json, @protocol)
+    end
+
+    test "Resource Object: Relationships Object: when it is not an object" do
+      document_json = %{
+        "data" => [%{
+          "id" => "91c4ca5a-beda-484e-bcd9-77b378aa48f3",
+          "type" => "people",
+          "relationships" => "foo"
+        }]
+      }
+
+      assert {:error, "The field 'relationships' must be an object."} = Parser.parse(document_json, @protocol)
+    end
+
+    test "Resource Object: Relationships Object: when mandatory fields are missing" do
+      document_json = %{
+        "data" => [%{
+          "id" => "91c4ca5a-beda-484e-bcd9-77b378aa48f3",
+          "type" => "people",
+          "relationships" => %{
+            "author" => %{
+            }
+          }
+        }]
+      }
+
+      assert {:error, "A 'author' MUST contain at least one of the following members: links, data, meta"} = Parser.parse(document_json, @protocol)
+    end
+
+    test "Resource Object: Relationships Object: supports links and meta" do
+      document_json = %{
+        "data" => [%{
+          "id" => "91c4ca5a-beda-484e-bcd9-77b378aa48f3",
+          "type" => "people",
+          "relationships" => %{
+            "author" => %{
+              "links" => %{
+                "self" => "http://example.com/articles/1/relationships/author",
+                "related" => "http://example.com/articles/1/author"
+              },
+              "meta" => %{
+                 "copyright" => "Copyright 2015 Example Corp."
+              }
+            }
+          }
+        }]
+      }
+
+      assert {:ok, %Document{data: [%Resource{
+        id: "91c4ca5a-beda-484e-bcd9-77b378aa48f3",
+        type: "people",
+        relationships: %{
+          "author" => %Relationship{
+            links: %Links{
+              self: "http://example.com/articles/1/relationships/author",
+              related: "http://example.com/articles/1/author"
+            },
+            meta: %{
+               "copyright" => "Copyright 2015 Example Corp."
+            }
+          }
+        },
+      }]}} = Parser.parse(document_json, @protocol)
+    end
+
+    test "Resource Object: Relationships Object: support Resource Identifier as a single object" do
+      document_json = %{
+        "data" => [%{
+          "id" => "91c4ca5a-beda-484e-bcd9-77b378aa48f3",
+          "type" => "people",
+          "relationships" => %{
+            "author" => %{
+              "data" => %{
+                "type" => "people",
+                "id" => "9",
+                "meta" => %{"copyright" => "Copyright 2015 Example Corp."} }
+            }
+          }
+        }]
+      }
+
+      assert {:ok, %Document{data: [%Resource{
+        id: "91c4ca5a-beda-484e-bcd9-77b378aa48f3",
+        type: "people",
+        relationships: %{
+          "author" => %Relationship{
+            data: %ResourceIdentifier{
+               id: "9",
+               type: "people",
+               meta: %{
+                 "copyright" => "Copyright 2015 Example Corp."
+               }
+            }
+          }
+        },
+      }]}} = Parser.parse(document_json, @protocol)
+    end
+
+    test "Resource Object: Relationships Object: support Resource Identifier as an array" do
+      document_json = %{
+        "data" => [%{
+          "id" => "91c4ca5a-beda-484e-bcd9-77b378aa48f3",
+          "type" => "people",
+          "relationships" => %{
+            "author" => %{
+              "data" => [%{
+                "type" => "people",
+                "id" => "9",
+                "meta" => %{"copyright" => "Copyright 2015 Example Corp."}
+              }]
+            }
+          }
+        }]
+      }
+
+      assert {:ok, %Document{data: [%Resource{
+        id: "91c4ca5a-beda-484e-bcd9-77b378aa48f3",
+        type: "people",
+        relationships: %{
+          "author" => %Relationship{
+            data: [%ResourceIdentifier{
+               id: "9",
+               type: "people",
+               meta: %{
+                 "copyright" => "Copyright 2015 Example Corp."
+               }
+            }]
+          }
+        },
+      }]}} = Parser.parse(document_json, @protocol)
+    end
+
+    test "Resource Object: Relationships Object: support Resource Identifier as an empty array" do
+      document_json = %{
+        "data" => [%{
+          "id" => "91c4ca5a-beda-484e-bcd9-77b378aa48f3",
+          "type" => "people",
+          "relationships" => %{
+            "author" => %{
+              "data" => []
+            }
+          }
+        }]
+      }
+
+      assert {:ok, %Document{data: [%Resource{
+        id: "91c4ca5a-beda-484e-bcd9-77b378aa48f3",
+        type: "people",
+        relationships: %{
+          "author" => %Relationship{
+            data: []
+          }
+        },
+      }]}} = Parser.parse(document_json, @protocol)
+    end
+
+    test "Resource Object: Relationships Object: support Resource Identifier as nil" do
+      document_json = %{
+        "data" => [%{
+          "id" => "91c4ca5a-beda-484e-bcd9-77b378aa48f3",
+          "type" => "people",
+          "relationships" => %{
+            "author" => %{
+              "data" => nil
+            }
+          }
+        }]
+      }
+
+      assert {:ok, %Document{data: [%Resource{
+        id: "91c4ca5a-beda-484e-bcd9-77b378aa48f3",
+        type: "people",
+        relationships: %{
+          "author" => %Relationship{
+            data: nil
+          }
+        },
+      }]}} = Parser.parse(document_json, @protocol)
     end
   end
 end
